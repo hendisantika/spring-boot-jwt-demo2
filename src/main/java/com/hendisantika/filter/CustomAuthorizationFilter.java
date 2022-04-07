dca35e9b3a7732d8c0c6fc3d0b1914c237b977b6
Hendi Santika <hendisantika@gmail.com>
2022-04-07 10:28:28 +0700

    #15 - Add CustomAuthorizationFilter

---
+++ src/main/java/com/hendisantika/filter/CustomAuthorizationFilter.java
@@ -0,0 +1,17 @@
+package com.hendisantika.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/04/22
+ * Time: 10.28
+ */
+@Slf4j
+public class CustomAuthorizationFilter extends OncePerRequestFilter {
+}
