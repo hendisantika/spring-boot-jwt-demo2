27bd4ac034074ba29c5577a950fb61d7ebc39ff1
Hendi Santika <hendisantika@gmail.com>
2022-04-07 10:31:25 +0700

    #18 - Add UserServiceImpl

---
+++ src/main/java/com/hendisantika/service/UserServiceImpl.java
@@ -0,0 +1,29 @@
+package com.hendisantika.service;
+
+import com.hendisantika.repository.RoleRepository;
+import com.hendisantika.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/04/22
+ * Time: 10.30
+ */
+@Service
+@RequiredArgsConstructor
+@Transactional
+@Slf4j
+public class UserServiceImpl implements UserService, UserDetailsService {
+    private final UserRepository userRepo;
+    private final RoleRepository roleRepo;
+    private final PasswordEncoder passwordEncoder;
+}
