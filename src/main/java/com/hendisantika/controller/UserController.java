bd625549aa4c50f974823c5c9e200000b8e10b25
Hendi Santika <hendisantika@gmail.com>
2022-04-07 10:35:02 +0700

    #27 - Add getUsers in UserController

--- src/main/java/com/hendisantika/controller/UserController.java
+++ src/main/java/com/hendisantika/controller/UserController.java
@@ -2,6 +2,8 @@ package com.hendisantika.controller;
 
 import com.hendisantika.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -19,4 +21,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+
+    @GetMapping("/users")
+    public ResponseEntity<List<User>> getUsers() {
+        return ResponseEntity.ok().body(userService.getUsers());
+    }
 }
