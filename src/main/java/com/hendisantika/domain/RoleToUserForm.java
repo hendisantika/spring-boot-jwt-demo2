5e1f66e6d064b3d25dac322e068b83824079bde7
Hendi Santika <hendisantika@gmail.com>
2022-04-07 10:33:59 +0700

    #25 - Add RoleToUserForm Domain

---
+++ src/main/java/com/hendisantika/domain/RoleToUserForm.java
@@ -0,0 +1,18 @@
+package com.hendisantika.domain;
+
+import lombok.Data;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/04/22
+ * Time: 10.33
+ */
+@Data
+class RoleToUserForm {
+    private String username;
+    private String roleName;
+}
