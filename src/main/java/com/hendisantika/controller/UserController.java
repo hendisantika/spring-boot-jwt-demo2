323776228ec1c61e30f656b976656025d218374f
Hendi Santika <hendisantika@gmail.com>
2022-04-07 10:34:43 +0700

    #26 - Add UserController

---
+++ src/main/java/com/hendisantika/controller/UserController.java
@@ -0,0 +1,22 @@
+package com.hendisantika.controller;
+
+import com.hendisantika.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/04/22
+ * Time: 10.34
+ */
+@RestController
+@RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+    private final UserService userService;
+}
