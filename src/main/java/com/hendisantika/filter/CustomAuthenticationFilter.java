ef13c996d8d997489c3e42d7a3f08c061603c9e6
Hendi Santika <hendisantika@gmail.com>
2022-04-07 10:27:25 +0700

    #13 - Add attemptAuthentication in CustomAuthenticationFilter

--- src/main/java/com/hendisantika/filter/CustomAuthenticationFilter.java
+++ src/main/java/com/hendisantika/filter/CustomAuthenticationFilter.java
@@ -2,7 +2,13 @@ package com.hendisantika.filter;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,5 +25,16 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
     public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
         this.authenticationManager = authenticationManager;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        String username = request.getParameter("username");
+        String password = request.getParameter("password");
+        log.info("Username is: {}", username);
+        log.info("Password is: {}", password);
+        UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(username,
+                password);
+        return authenticationManager.authenticate(authenticationToken);
     }
 }
