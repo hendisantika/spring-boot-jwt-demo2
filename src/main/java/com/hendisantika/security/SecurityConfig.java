84e425a9cd09e441da1d9710341f89f10abed12c
Hendi Santika <hendisantika@gmail.com>
2022-04-07 10:25:35 +0700

    #9 - Add configure in SecurityConfig

--- src/main/java/com/hendisantika/security/SecurityConfig.java
+++ src/main/java/com/hendisantika/security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.hendisantika.security;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -23,4 +24,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final UserDetailsService userDetailsService;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
 
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService).passwordEncoder(bCryptPasswordEncoder);
+    }
 }
