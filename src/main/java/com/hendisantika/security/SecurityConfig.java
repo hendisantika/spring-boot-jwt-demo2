30e601770d51f488a5fc3017457ba974e7ff2acd
Hendi Santika <hendisantika@gmail.com>
2022-04-07 10:26:26 +0700

    #11 - Add authenticationManagerBean in SecurityConfig

--- src/main/java/com/hendisantika/security/SecurityConfig.java
+++ src/main/java/com/hendisantika/security/SecurityConfig.java
@@ -1,7 +1,9 @@
 package com.hendisantika.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -48,5 +50,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http.authorizeRequests().anyRequest().authenticated();
         http.addFilter(customAuthenticationFilter);
         http.addFilterBefore(new CustomAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 }
