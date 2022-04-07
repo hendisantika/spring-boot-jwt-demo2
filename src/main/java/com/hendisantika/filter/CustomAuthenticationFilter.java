967f9bffd7a74850530611f7a07ee611df324dd3
Hendi Santika <hendisantika@gmail.com>
2022-04-07 10:26:55 +0700

    #12 - Add CustomAuthenticationFilter

---
+++ src/main/java/com/hendisantika/filter/CustomAuthenticationFilter.java
@@ -0,0 +1,23 @@
+package com.hendisantika.filter;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-jwt-demo2
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 07/04/22
+ * Time: 10.26
+ */
+@Slf4j
+public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    private final AuthenticationManager authenticationManager;
+
+    public CustomAuthenticationFilter(AuthenticationManager authenticationManager) {
+        this.authenticationManager = authenticationManager;
+    }
+}
